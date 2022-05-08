72cbc7af517e39bd05930b53cdb18969d27e01a9
sixiaolin <sixiaolin@thinktech.net.cn>
2022-05-08 16:00:03 +0800

    大数运算基础模块

--- cmathematics/main.c
+++ cmathematics/main.c
@@ -2,16 +2,11 @@
 #include "cmathematics.h"
 #include "vec.h"
 #include "matrix.h"
+#include "bigint.h"
 
 int main()
 {
-    mat m = newMatrixMat(3, 3,
-                         7.0f, 2.0f, 0.0f,
-                         3.0f, -15.0f, 0.0f,
-                         6.0f, -4.0f, 2.0f);
-    printMat(m);
-
-    printf("%f\n", determinant(m));
+    bigint b = strToBigint("546573");
 
     return 0;
 }
